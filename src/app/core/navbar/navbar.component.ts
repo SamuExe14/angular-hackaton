b96de48006a5adb4c81e8b207b809b5810848518
SamuExe14 <samueleleoonardi14@gmail.com>
2023-12-15 15:42:52 +0100

    Modificato gitignore

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -3,22 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: `
-     <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
       <a class="navbar-brand" href="#">Navbar</a>
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav  ">
-            <a class="nav-item nav-link "  routerLinkActive ="active" href="#" routerLink="/home">Home</a>
-            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/users" >M2D</a>
-            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/contacts">Darwin</a>
+            <a
+              class="nav-item nav-link "
+              routerLinkActive="active"
+              href="#"
+              routerLink="/home"
+              >Home</a
+            >
+            <a
+              class="nav-item nav-link "
+              routerLinkActive="active"
+              href="#"
+              routerLink="/users"
+              >M2D</a
+            >
+            <a
+              class="nav-item nav-link "
+              routerLinkActive="active"
+              href="#"
+              routerLink="/contacts"
+              >Darwin</a
+            >
           </div>
         </div>
       </div>
     </nav>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
