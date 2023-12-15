795db78ad8394c9a1e5d1334a5192a8f40a25de0
SamuExe14 <samueleleoonardi14@gmail.com>
2023-12-15 15:50:24 +0100

    Modificato cardsComponent

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cards',
   template: `
-    <p>
-      cards works!
-    </p>
+    <div class="g"></div>
   `,
   styles: [
   ]
