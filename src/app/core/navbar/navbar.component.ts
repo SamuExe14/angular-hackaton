1afc1b90d8e9b9fd795f56308eb47e634a3e7475
antonio <antonioplatania04@gmail.com>
2023-12-15 15:50:21 +0100

    navbar aggiornato

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -4,13 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-navbar',
   template: `
      <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#"></a>
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav  ">
             <a class="nav-item nav-link "  routerLinkActive ="active" href="#" routerLink="/home">Home</a>
-            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/users" >M2D</a>
-            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/contacts">Darwin</a>
+            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/" >M2D</a>
+            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/">Darwin</a>
+            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/">Darwin</a>
+            <a class="nav-item nav-link " routerLinkActive ="active" href="#" routerLink="/">Darwin</a>
+
           </div>
         </div>
       </div>
